3372c6d1b68d1514b2580f957e4fe4f023b77211
Serafima <serafima93.93@gmail.com>
2021-03-31 18:22:31 +0300

    Начала работать над 3-м заданием-2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import {createSiteMenuTemplate} from './view/menu.js';
 import {createUser} from './view/user.js';
+import {createFilmCard} from './view/film-card.js';
+import {createShowMoreButton} from './view/button-show-more.js';
+import {createPopUp} from './view/pop-up-information.js';
 
+
+const CARDS_COUNT = 5;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -13,3 +18,18 @@ render(siteUserElement, createUser(), 'beforeend');
 // добавляю меню
 const siteMainElement = document.querySelector('.main');
 render(siteMainElement, createSiteMenuTemplate(), 'beforeend');
+
+// добавляю карточку
+
+for (let i = 0; i < CARDS_COUNT; i++) {
+  const siteMainElement = document.querySelector('.main');
+  render(siteMainElement, createFilmCard(), 'beforeend');
+}
+
+// добавляю кнопку
+render(siteMainElement, createShowMoreButton(), 'beforeend');
+
+//поп-ап
+render(siteMainElement, createPopUp(), 'beforeend');
+
+
