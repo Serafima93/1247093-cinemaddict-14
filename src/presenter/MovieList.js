b4d7860a6cf9700634449bcc9376bbe0a06ca2f0
Serafima <serafima93.93@gmail.com>
2021-04-20 23:39:16 +0300

    доработка

--- src/presenter/MovieList.js
+++ src/presenter/MovieList.js
@@ -8,80 +8,74 @@ import { FilmCard } from '../view/film-card.js';
 import { ShowMoreButton } from '../view/button-show-more.js';
 import { PopUp } from '../view/pop-up-information.js';
 import { render, replaceChild, remove } from '../utils/utils-render.js';
+// import { updateItem } from '../utils/utils-common.js';
 
-const FILMS_MAX_COUNT = 20;
+// const FILMS_MAX_COUNT = 20;
 const FILMS_MIN_COUNT = 2;
 const FILM_COUNT_PER_STEP = 5;
+
 
 class FilmBoard {
   constructor(boardContainer) {
     this._boardContainer = boardContainer;
+    this._renderedFilmCount = FILM_COUNT_PER_STEP;
 
-    this._filmPresenter = {};
-    // this._filmPresenter[this._films[0].id]; - фильмы по айди - но у меня сразу все фильмы. Вставляем в цикл?
-
-    this._boardComponent = new FilmList();
-
+    this._filmListComponent = new FilmList();
     this._noFilmsComponent = new EmptyWrap();
     this._sortComponents = new Sort();
     this._loadMoreButtonComponent = new ShowMoreButton();
 
-    this._renderedFilmCount = FILM_COUNT_PER_STEP;
     this._handleLoadMoreButtonClick = this._handleLoadMoreButtonClick.bind(this);
   }
-
-
   init(films) {
     this._films = films.slice();
 
-    // в классе рисуем контейнер или как?
-    // render(this._boardContainer, this._boardComponent);
-
+    this._filmPresenter = {};
     this._renderFilmBoard();
   }
 
-  _renderSort() {
-    // Метод для рендеринга сортировки
-    render(this._boardContainer, this._sortComponents);
-  }
+  _renderFilmBoard() {
+    this._renderMenu(this._films);
 
-  _renderMenu(films) {
-    const favoritFilm = films.filter((film) => film.isFavorit).length;
-    const watchedFilm = films.filter((film) => film.isWatched).length;
-    const futureFilm = films.filter((film) => film.futureFilm).length;
+    if (!this._films.length) {
+      this._renderNoFilms();
+      const filmRemove = this._boardContainer.querySelector('.sort');
+      filmRemove.classList.add('visually-hidden');
 
-    render(this._boardContainer, new SiteMenu(favoritFilm, watchedFilm, futureFilm));
-  }
-
-
-  _clearTaskList() { // мне нужно вызвать его в начале цикла следующего метода при клике?
-    Object
-      .values(this._boardComponent) //
-      .forEach((presenter) => presenter.remove(this._boardComponent)); //   подходит ли мне? у меня же уже список
-    this._filmPresenter = {};
-    this._renderedFilmCount = FILM_COUNT_PER_STEP;
-    remove(this._loadMoreButtonComponent);
-  }
-
-  _buttonsClickHandler(filmView) {
-    filmView.favoriteClickHandler(() => {
-      // const filmIdHistoryNumber = filmView._film.id;
-      filmView._film.isFavorit = true; // создаю на фильме флаг - верно
-    });
-  }
-
-  _renderFilms(container, films, count) {
-    for (let i = 0; i < this._films.slice(0, count).length; i++) {
-      const filmView = new FilmCard(films[i]);
-      render(container, filmView);
-      this._renderFilmListner(filmView, films[i]);
-      this._buttonsClickHandler(filmView);
+    } else {
+      this._renderContainers();
     }
+
   }
 
-  _renderFilmsMain() {
+  _renderContainers() {
+    render(this._boardContainer, this._sortComponents); // сортировка фильмов
+    render(this._boardContainer, this._filmListComponent); // отрисовываем сам контейнер new FilmList()
+    this._renderFilmList();
+    this._renderFilmAdditionalList();
+  }
+
+  _renderLoadMoreButton() {
+    const buttonPlace = this._boardContainer.querySelector('.films-list');
+    render(buttonPlace, this._loadMoreButtonComponent);
+    this._loadMoreButtonComponent.setClickHandler(this._handleLoadMoreButtonClick);
+  }
+
+  _handleLoadMoreButtonClick() {
     const filmCardContainers = this._boardContainer.querySelector('#main-container');
-    this._renderFilms(filmCardContainers, this._films, FILM_COUNT_PER_STEP);
+    this._films
+      .slice(this._renderedFilmCount, this._renderedFilmCount + FILM_COUNT_PER_STEP)
+      .forEach((film) => {
+        const newFilm = new FilmCard(film);
+        render(filmCardContainers, newFilm);
+        this._renderFilmListner(newFilm, film);
+
+        this._buttonsClickHandler(newFilm);
+      });
+    this._renderedFilmCount += FILM_COUNT_PER_STEP;
+    if (this._renderedFilmCount >= this._films.length) {
+      remove(this._loadMoreButtonComponent);
+    }
   }
 
   _renderPopUp(film) {
@@ -111,33 +105,26 @@ class FilmBoard {
     document.addEventListener('keydown', onEscKeyDown);
   }
 
-  _renderFilmListner(film, filmPopUp) {
-    film.setEditClickHandler(() => {
-      this._renderPopUp(filmPopUp);
-    });
-  }
-
-  _handleLoadMoreButtonClick() {
-    const filmCardContainers = this._boardContainer.querySelector('#main-container');
-    this._films
-      .slice(this._renderedFilmCount, this._renderedFilmCount + FILM_COUNT_PER_STEP)
-      .forEach((film) => {
-        const newFilm = new FilmCard(film);
-        render(filmCardContainers, newFilm);
-        this._renderFilmListner(newFilm, film);
-
-        this._buttonsClickHandler(newFilm);
-      });
-    this._renderedFilmCount += FILM_COUNT_PER_STEP;
-    if (this._renderedFilmCount >= this._films.length) {
-      remove(this._loadMoreButtonComponent);
+  _renderFilms(container, films, count) {
+    for (let i = 0; i < films.slice(0, count).length; i++) {
+      const filmView = new FilmCard(films[i]);
+      render(container, filmView);
+      this._renderFilmListner(filmView, films[i]);
+      this._buttonsClickHandler(filmView);
     }
   }
+  /*Мне нужно передать данные из вью в презентер, и тот должен изменить моки
+  Узнаем фильм по id - по нажатию на кнопку (пр. фаворит) - получаем его. Но что потом?
+Карточка должна сообщить что она была нажата и что надо пребавить к списку вверху фильм?
+Или же мы нажатой карточке ставим флаг - тру в позиции фейфорит и перерисовываем весь список?
+если так, то нам нужно написать - если позиция меняется, то тогда все карточки перерисовываются
+(только данные - не новый вызов функции) - для этого их надо стереть и рисуются заново?
+  */
 
-  _renderLoadMoreButton() {
-    const buttonPlace = this._boardContainer.querySelector('.films-list');
-    render(buttonPlace, this._loadMoreButtonComponent);
-    this._loadMoreButtonComponent.setClickHandler(this._handleLoadMoreButtonClick);
+  _renderFilmsMain() {
+    const filmCardContainers = this._boardContainer.querySelector('#main-container');
+    this._renderFilms(filmCardContainers, this._films, FILM_COUNT_PER_STEP);
+    // this._filmPresenter[film.id] = filmPresenter;
   }
 
   _renderFilmList() {
@@ -146,6 +133,41 @@ class FilmBoard {
     if (this._films.length > FILM_COUNT_PER_STEP) {
       this._renderLoadMoreButton();
     }
+  }
+
+  _renderFilmListner(film, filmPopUp) {
+    film.setEditClickHandler(() => {
+      this._renderPopUp(filmPopUp);
+    });
+  }
+
+  _buttonsClickHandler(filmView) {
+    filmView.favoriteClickHandler(() => {
+      // const filmIdHistoryNumber = filmView._film.id;
+      filmView._film.isFavorit = true; // создаю на фильме флаг - верно
+    });
+  }
+
+
+  _favoriteClickHandler() { Object.assign({}, this._films, { isFavorit: !this._films.isFavorit });
+  }
+
+  _clearTaskList() { // мне нужно вызвать его в начале цикла следующего метода при клике?
+    Object
+      .values(this._filmListComponent) //
+      .forEach((presenter) => presenter.remove(this._filmListComponent));
+    this._filmPresenter = {};
+    this._renderedFilmCount = FILM_COUNT_PER_STEP;
+    remove(this._loadMoreButtonComponent);
+  }
+
+
+  _renderMenu(films) {
+    const favoritFilm = films.filter((film) => film.isFavorit).length;
+    const watchedFilm = films.filter((film) => film.isWatched).length;
+    const futureFilm = films.filter((film) => film.futureFilm).length;
+
+    render(this._boardContainer, new SiteMenu(favoritFilm, watchedFilm, futureFilm));
   }
 
   _renderFilmAdditionalList() {
@@ -160,44 +182,10 @@ class FilmBoard {
 
     this._renderFilms(filmCardContainerMostRate, rateFilm, FILMS_MIN_COUNT);
     this._renderFilms(filmCardContainerMostComments, commentsFilm, FILMS_MIN_COUNT);
-
-    // for (let i = 0; i < FILMS_MIN_COUNT; i++) {
-    //   const filmRate = new FilmCard(rateFilm[i]);
-    //   const filmComments = new FilmCard(commentsFilm[i]);
-
-    //   render(filmCardContainerMostRate, filmRate);
-    //   render(filmCardContainerMostComments, filmComments);
-    //   this._renderFilmListner(filmRate, rateFilm[i]);
-    //   this._renderFilmListner(filmComments, commentsFilm[i]);
-
-    //   this._buttonsClickHandler(filmRate);
-    //   this._buttonsClickHandler(filmComments); // добавить удаление полных дублей - отмена нажатия дважды
-    // }
   }
 
   _renderNoFilms() {
     render(this._boardContainer, this._noFilmsComponent);
-  }
-
-  _renderFilmBoard() {
-    // render(this._boardContainer,this._boardComponent);
-    // рендер сортировки. Рендер меню. Рендер самих фильмов. Рендер мини контейнеров.
-
-
-    this._renderMenu(this._films); // запуск меню. Тут передается постоянно с полными данными
-
-    if (FILMS_MAX_COUNT === 0) {
-      this._renderNoFilms();
-      const filmRemove = this._boardContainer.querySelector('.sort');
-      filmRemove.classList.add('visually-hidden');
-
-    } else {
-      this._renderSort();
-      render(this._boardContainer, this._boardComponent); // отрисовываем сам контейнер new FilmList()
-    }
-    this._renderFilmList();
-    this._renderFilmAdditionalList();
-
   }
 }
 export { FilmBoard };
